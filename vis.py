9e2cadef29d10279ca4b5fbd344b13ab5b76ba9c
Ray Haleblian <ray@haleblian.com>
2009-11-08 15:52:35 +1100

    Remove broken by-person_id lookup.

--- vis.py
+++ vis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import urllib2
+import xml.dom
 import xml.dom.minidom as xml
 import oa, lexi
 
@@ -8,19 +9,6 @@ result = oa.request( "getRepresentatives" )
 dom = xml.parseString(result)
 representatives = dom.getElementsByTagName("match")
 print "Num Representatives: %d" % len(representatives)
-for rep in representatives:
-    print rep
-
-# get a representative's id
-result = oa.request( "getRepresentatives", {"search":"Kevin%20Rudd"} )
-print result
-dom = xml.parseString(result)
-elem = dom.getElementsByTagName("person_id").item(0)
-#for child in elem.childNodes:
-    #if child.nodeType == xml.TEXT_NODE:
-        #id = child.data
-        #break
-#print id
 
 # exercise term counting.
 for term in ["promiscuity","affair","web","broadband"]:
