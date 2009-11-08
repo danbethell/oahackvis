868d8b33b3e1af18869c06866ad7eb6b5ea97e6d
Ray Haleblian <ray@haleblian.com>
2009-11-08 13:05:43 +1100

    Remove API key from oa and assume there is a local key module containing it.

--- oa.py
+++ oa.py
@@ -1,15 +1,18 @@
 import urllib2
+import key
 
-api_key = "BQZw3gBCok7mGHLW9kB5t65S"
 request_root = "http://www.openaustralia.org/api/"
 
 # request some information
-def request( function, params={}):
+def request( function, params={}, dom=False):
     global api_key, request_root
-    plist = ['key=%s' % api_key,'output=xml']
+    plist = ['key=%s' % key.api_key,'output=xml']
     for key in params.keys():
         plist.append('%s=%s' % (key,params[key]))
     request = "%s%s?%s" % ( request_root, function, '&'.join(plist) )
     response = urllib2.urlopen( request )
     result = response.read()
+    if dom:
+        doc = dom.parseString(result)
+        return doc
     return result
