5ac3b3af4cb491c9da8cbbdd959671eaf8a075c0
Ray Haleblian <ray@haleblian.com>
2009-11-08 16:05:55 +1100

    Move lexi code to oa. Add Makefile for cleaning.

--- oa.py
+++ oa.py
@@ -12,6 +12,44 @@ def request( function, params={}, dom=False):
     response = urllib2.urlopen( request )
     result = response.read()
     if dom:
-        doc = dom.parseString(result)
+        doc = xml.dom.minidom.parseString(result)
         return doc
     return result
+
+def termCount(term,person=None,debates=True,comments=True,hansard=True):
+    '''
+    Return count of records where term was found.
+    Not the same as the total frequency of term for th record.
+    '''
+    result = ''
+    count = []
+    parms = {"search":term}
+    if person:
+        parms.update({"person":person})
+    if hansard:
+        result = request( "getHansard", parms)
+        dom = xml.parseString(result)
+        count += dom.getElementsByTagName("match")
+    if debates:
+        parms.update({"type":"representatives"})
+        result = request( "getDebates", parms)
+        dom = xml.parseString(result)
+        count += dom.getElementsByTagName("match")
+        parms.update({"type":"senate"})
+        result = request( "getDebates", parms)
+        dom = xml.parseString(result)
+        count += dom.getElementsByTagName("match")
+    if comments:
+        result = request( "getComments", parms)
+        dom = xml.parseString(result)
+        count += dom.getElementsByTagName("match")
+    return len(count)
+
+
+if __name__ == '__main__':
+    for term in ['transport','economy','promiscuity','broadband','alcohol','crowdsourcing']:
+        print '%s total %s' % (term, termCount(term))
+        print '%s debates %s' % (term, termCount(term, comments=False, hansard=False))
+        print '%s hansard %s' % (term, termCount(term, debates=False, comments=False))
+        print '%s comments %s' % (term, termCount(term, hansard=False, debates=True))
+
--- vis.py
+++ vis.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import urllib2
-import xml.dom
 import xml.dom.minidom as xml
-import oa, lexi
+import oa
 
 # get representatives
 result = oa.request( "getRepresentatives" )
@@ -12,5 +11,6 @@ print "Num Representatives: %d" % len(representatives)
 
 # exercise term counting.
 for term in ["promiscuity","affair","web","broadband"]:
-    print 'Term %s: %d' % (term,lexi.termCount(term))
-    print "Term %s in %s: %d" % (term, "hansards", lexi.termCount(term, debates=False, comments=False))
+    print 'Term %s: %d' % (term,oa.termCount(term))
+    print "Term %s in %s: %d" % (term, "hansards", oa.termCount(term, debates=False, comments=False))
+
