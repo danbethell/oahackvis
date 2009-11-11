38e6274d35a7fd3a624799a0b89cda1b99724010
Dan Bethell <dan@pawfal.org>
2009-11-11 19:43:23 +1100

    Added some information to README. Moved the key stuff into the top of oa.py. Tidied up the top-level methods in vis.py

--- oa.py
+++ oa.py
@@ -1,23 +1,82 @@
 import urllib2
 import xml.dom.minidom as xml
-import key
 
+api_key = "YOUR_OPEN_AUSTRALIA_API_KEY_HERE"
 request_root = "http://www.openaustralia.org/api/"
 
-# request some information
+def getAPIKey():
+    '''
+    Returns the API key
+    '''
+    global api_key
+    return api_key
+
+def getRequestRoot():
+    '''
+    Returns the OpenAustralia api http root
+    '''
+    global request_root
+    return request_root
+
 def request( function, params={}):
-    plist = ['key=%s' % key.getAPIKey(),'output=xml']
+    '''
+    Make a call to the OpenAustralia API
+    Returns a string of xml suitable for parsing to minidom
+    '''
+    plist = ['key=%s' % getAPIKey(),'output=xml']
     for k in params.keys():
         plist.append('%s=%s' % (k,params[k]))
-    request = "%s%s?%s" % ( request_root, function, '&'.join(plist) )
+    request = "%s%s?%s" % ( getRequestRoot(), function, '&'.join(plist) )
     response = urllib2.urlopen( request )
     result = response.read()
     return result
 
+def getText( rep, key ):
+    '''
+    Gets some text from a dom element
+    '''
+    element = rep.getElementsByTagName( key )[0]
+    nodelist = element.childNodes
+    result = ""
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            result = result + node.data
+    return result
+
+def getRepresentatives():
+    '''
+    Return the representatives as a list of dictionaries
+    '''
+    result = request( "getRepresentatives" )
+    dom = xml.parseString(result)
+    representatives = dom.getElementsByTagName("match")
+
+    # format xml representatives into a list of 'rep' dictionaries
+    reps = []
+    for r in representatives:
+        rep = {}
+        rep['member_id'] = getText(r, "person_id")
+        rep['person_id'] = getText(r, "person_id")
+        rep['name'] = getText(r, "name")
+        rep['party'] = getText(r, "party")
+        rep['constituency'] = getText(r, "constituency")
+        reps.append(rep)
+    return reps
+
+def numDebates(person):
+    '''
+    Returns the total number of debates for the specified person
+    '''
+    params = {"type":"representatives", "person":person}
+    result = request( "getDebates", params )
+    dom = xml.parseString(result)
+    info = dom.getElementsByTagName("info")
+    return int(getText(dom, "total_results"))
+
 def termCount(term,person=None,debates=True,comments=True,hansard=True):
     '''
     Return count of records where term was found.
-    Not the same as the total frequency of term for th record.
+    Not the same as the total frequency of term for the record.
     '''
     result = ''
     count = []
@@ -43,11 +102,14 @@ def termCount(term,person=None,debates=True,comments=True,hansard=True):
         count += dom.getElementsByTagName("match")
     return len(count)
 
-
+# some tests
 if __name__ == '__main__':
+    import vis
+    print "%d reps" % len(vis.reps)
+    for r in vis.reps[:10]:
+        print "Person %s, %s num debates: %d" % (r['person_id'], r['name'], numDebates(r['person_id']))
     for term in ['transport','economy','promiscuity','broadband','alcohol','crowdsourcing']:
-        print '%s total %s' % (term, termCount(term))
-        print '%s debates %s' % (term, termCount(term, comments=False, hansard=False))
-        print '%s hansard %s' % (term, termCount(term, debates=False, comments=False))
-        print '%s comments %s' % (term, termCount(term, hansard=False, debates=True))
-
+        print '%s total: %s' % (term, termCount(term))
+        print '%s debates: %s' % (term, termCount(term, comments=False, hansard=False))
+        print '%s hansard: %s' % (term, termCount(term, debates=False, comments=False))
+        print '%s comments: %s' % (term, termCount(term, hansard=False, debates=True))
--- vis.py
+++ vis.py
@@ -1,16 +1,63 @@
 #!/usr/bin/env python
-import urllib2
-import xml.dom.minidom as xml
+import random
 import oa
 
 # get representatives
-result = oa.request( "getRepresentatives" )
-dom = xml.parseString(result)
-representatives = dom.getElementsByTagName("match")
-print "Num Representatives: %d" % len(representatives)
+reps = oa.getRepresentatives()
 
-# exercise term counting.
-for term in ["promiscuity","affair","web","broadband"]:
-    print 'Term %s: %d' % (term,oa.termCount(term))
-    print "Term %s in %s: %d" % (term, "hansards", oa.termCount(term, debates=False, comments=False))
+def numRepresentatives():
+    '''
+    Returns the number of representatives
+    '''
+    return len(reps)
 
+def getName( index ):
+    '''
+    Returns the name of representative at index
+    '''
+    return reps[index]['name']
+
+assigned_colours = {}
+def getColour( index ):
+    '''
+    Returns a random colour for the party of representative at index
+    '''
+    global assigned_colours
+    party = reps[index]['party']
+    some_colours = [
+        (1,.5,.5),
+        (.5,1,.5),
+        (.5,.5,1),
+        (1,1,.5),
+        (.5,1,1),
+        (1,.5,1)
+        ]
+    if not party in assigned_colours:
+        assigned_colours[party] = some_colours[len(assigned_colours)%6]
+    return assigned_colours[party]
+
+def termCount( index, term ):
+    '''
+    Returns the number of time representive at index used 'term' in debates & hansard
+    '''
+    global reps
+    store_key = "termcount_%s" % term # store the result with each rep using this key
+    if not reps[index].has_key( store_key ):
+        reps[index][store_key] = oa.termCount( term, reps[index]['person_id'], debates=True, hansard=True, comments=False )
+    return reps[index][store_key]
+
+def numDebates( index ):
+    '''
+    Returns the number of debates for the reprentative at index
+    '''
+    if not reps[index].has_key("num_debates"):
+        reps[index]['num_debates'] = oa.numDebates( reps[index]['person_id'] )
+    return reps[index]['num_debates']
+
+# some tests
+if __name__ == '__main__':
+    print "%d reps" % numRepresentatives()
+    for i in range(0, 10):
+        print "Person %s, %s num debates: %d" % (reps[i]['person_id'], reps[i]['name'], numDebates(i))
+        print (reps[i]['party'], str(getColour(i)))
+        print "used the word 'web' %d times" % termCount(i, "web")
